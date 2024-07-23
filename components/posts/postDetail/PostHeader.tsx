677475a1979762ae00735668c93a2fa33fc46b23
pawelek <k.pawelek@vp.pl>
2024-07-23 23:43:20 +0200

    Update PostHeader.tsx

--- components/posts/postDetail/PostHeader.tsx
+++ components/posts/postDetail/PostHeader.tsx
@@ -1,3 +1,10 @@
-export const PostHeader = () => {
-  return <header></header>;
+import Image from "next/image";
+
+export const PostHeader = ({ title, image }) => {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Image src={image} alt={title} />
+    </header>
+  );
 };
