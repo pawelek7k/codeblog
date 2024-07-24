54635bcfdb5cac6eaf53fd1da2784ec120b946d0
pawelek <k.pawelek@vp.pl>
2024-07-24 17:31:38 +0200

    Update postsUtil.ts

--- lib/postsUtil.ts
+++ lib/postsUtil.ts
@@ -1,1 +1,51 @@
-const getAllPosts = () => { }
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+
+interface PostData {
+    slug: string;
+    title: string;
+    date: string;
+    isFeatured: boolean;
+    content: string;
+}
+
+const postsDirectory = path.join(process.cwd(), 'posts')
+
+const getPostData = (fileName: string): PostData => {
+    const filePath = path.join(postsDirectory, fileName)
+    const fileContent = fs.readFileSync(filePath, 'utf-8')
+    const { data, content } = matter(fileContent)
+
+    const postSlug = fileName.replace(/\.md$/, '') // removes the file extension
+    const postData: PostData = {
+        slug: postSlug,
+        title: data.title,
+        date: data.date,
+        isFeatured: data.isFeatured,
+        content
+    }
+    return postData
+}
+
+export const getAllPosts = (): PostData[] => {
+    const postFiles = fs.readdirSync(postsDirectory)
+
+    const allPosts = postFiles.map(postFile => {
+        return getPostData(postFile)
+    })
+
+    const sortedPosts = allPosts.sort((A, B) => {
+        return A.date > B.date ? -1 : 1
+    })
+
+    return sortedPosts
+}
+
+export const getFeaturedPosts = (): PostData[] => {
+    const allPosts = getAllPosts()
+
+    const featuredPosts = allPosts.filter(post => post.isFeatured)
+
+    return featuredPosts
+}
