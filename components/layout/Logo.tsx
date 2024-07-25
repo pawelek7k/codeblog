1a9d1efd79c7044a57d662b4762fb4f89a0db632
pawelek <k.pawelek@vp.pl>
2024-07-25 18:07:43 +0200

    add logo styles

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,3 +1,10 @@
 export const Logo = () => {
-  return <h1 className="text-lg font-semibold">{"</> CodeBlog"}</h1>;
+  return (
+    <h1 className="text-lg font-semibold">
+      {"</>"}{" "}
+      <span className="text-accent">
+        Code<span className="text-text">Blog</span>
+      </span>
+    </h1>
+  );
 };
