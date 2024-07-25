149d6a1459ba8505c12b346f37f3f0ee7129ece4
pawelek <k.pawelek@vp.pl>
2024-07-25 13:49:12 +0200

    add fixes

--- pages/AllPosts.tsx
+++ pages/AllPosts.tsx
@@ -1,26 +1,12 @@
 import { PostsGrid } from "@/components/posts/PostsGrid";
-import { getAllPosts } from "@/lib/postsUtil";
-import { GetStaticProps, InferGetStaticPropsType } from "next";
 
-type AllPostsProps = InferGetStaticPropsType<typeof getStaticProps>;
-
-const AllPosts: React.FC<AllPostsProps> = (props) => {
+const AllPosts = (props) => {
   return (
     <section>
       <h1>All Posts</h1>
       <PostsGrid posts={props.posts} />
     </section>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const allPosts = getAllPosts();
-
-  return {
-    props: {
-      posts: allPosts,
-    },
-  };
 };
 
 export default AllPosts;
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,7 +1,21 @@
+import { getFeaturedPosts } from "@/lib/postsUtil";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
 import AllPosts from "../AllPosts";
 
-const AllPostPage = () => {
-  return <AllPosts />;
+type AllPostsProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const AllPostPage: React.FC<AllPostsProps> = (props) => {
+  return <AllPosts posts={props.posts} />;
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const allPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
 };
 
 export default AllPostPage;
