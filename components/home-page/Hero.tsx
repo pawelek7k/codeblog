069374411d510aa28efda172306a72c724e7e277
pawelek <k.pawelek@vp.pl>
2024-07-25 18:53:24 +0200

    Update Hero.tsx

--- components/home-page/Hero.tsx
+++ components/home-page/Hero.tsx
@@ -1,13 +1,13 @@
 export const Hero = () => {
   return (
     <section className="bg-hero-bg bg-fixed bg-center bg-no-repeat bg-cover h-screen flex justify-center items-center">
-      <div className="bg-secondaryBg backdrop-blur-md w-1/3 h-1/3 rounded-2xl">
-        <div className="bg-background w-full">
-          <div className="bg-red"></div>
-          <div className="bg-yellow"></div>
-          <div className="bg-green"></div>
-        </div>
-        <h1 className="text-5xl text-center">Programming blog</h1>
+      <div className="bg-secondaryBg backdrop-blur-md w-[35rem] h-1/3 rounded-2xl">
+        <ul className="bg-background w-full list-none flex  m-0 rounded-t-2xl p-2 gap-2">
+          <li className="bg-primary w-4 h-4 rounded-full"></li>
+          <li className="bg-secondary w-4 h-4 rounded-full"></li>
+          <li className="bg-accent w-4 h-4 rounded-full"></li>
+        </ul>
+        <h1 className="text-5xl text-center mt-24">Programming blog</h1>
       </div>
     </section>
   );
