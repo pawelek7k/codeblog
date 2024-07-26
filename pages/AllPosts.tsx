530b8914461251e96e3540bd87a3545b92bdceb2
pawelek <k.pawelek@vp.pl>
2024-07-26 19:00:35 +0200

    Update AllPosts.tsx

--- pages/AllPosts.tsx
+++ pages/AllPosts.tsx
@@ -1,6 +1,11 @@
 import { PostsGrid } from "@/components/posts/PostsGrid";
+import { PostProps } from "@/types";
 
-const AllPosts = ({ posts }) => {
+interface Props {
+  posts: PostProps[];
+}
+
+const AllPosts = ({ posts }: Props) => {
   return (
     <section className="p-24">
       <h1 className="text-2xl md:text-4xl">All Posts</h1>
