6a3c4de392083e19751f929e61a917e2f9f8812d
pawelek <k.pawelek@vp.pl>
2024-07-27 16:36:01 +0200

    connect to mongodb

--- pages/api/Contact.ts
+++ pages/api/Contact.ts
@@ -1,4 +1,4 @@
-
+import { Db, MongoClient } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { z } from 'zod';
 
@@ -6,33 +6,41 @@ const messageSchema = z.object({
     email: z.string().email('Invalid email address.'),
     name: z.string().min(1, 'Name is required.'),
     message: z.string().min(1, 'Message is required.')
-})
+});
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
         try {
-            const parsedBody = messageSchema.parse(req.body)
+            const parsedBody = messageSchema.parse(req.body);
 
             const newMessage = {
                 email: parsedBody.email,
                 name: parsedBody.name,
                 message: parsedBody.message
-            }
+            };
 
+            const client = await MongoClient.connect(process.env.MONGODB_URI || '');
 
-            res.status(201).json({ message: 'Successfully stored message!', newMessage })
+            const db: Db = client.db();
+
+            const result = await db.collection('messages').insertOne(newMessage);
+            newMessage._id = result.insertedId;
+
+            await client.close();
+
+            res.status(201).json({ message: 'Message successfully saved!', newMessage });
         } catch (e) {
             if (e instanceof z.ZodError) {
-                res.status(422).json({ message: 'Invalid input.', errors: e.errors })
+                res.status(422).json({ message: 'Invalid input.', errors: e.errors });
             } else {
-                res.status(500).json({ message: 'Internal server error.' })
+                console.error(e);
+                res.status(500).json({ message: 'Internal server error.' });
             }
         }
-
     } else {
-        res.setHeader('Allow', ['POST'])
-        res.status(405).end(`Method ${req.method} not allowed.`)
+        res.setHeader('Allow', ['POST']);
+        res.status(405).end(`Method ${req.method} not allowed.`);
     }
-}
+};
 
-export default handler
+export default handler;
