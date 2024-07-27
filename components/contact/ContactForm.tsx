4f62c6ff446c20e1ec3f20c7a912298ecc435492
pawelek <k.pawelek@vp.pl>
2024-07-27 13:21:45 +0200

    add form

---
+++ components/contact/ContactForm.tsx
@@ -0,0 +1,22 @@
+export const ContactForm = () => {
+  return (
+    <section className="p-24">
+      <h1 className="text-2xl md:text-4xl">How can I help you?</h1>
+      <form action="">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" required />
+        </div>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" required />
+        </div>
+        <div>
+          <label htmlFor="message">Email</label>
+          <textarea id="message" required rows="5"></textarea>
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
+  );
+};
